7494126a7aef9480d253779b2138e523f3e6051d
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-07 15:36:34 +0530

    Added changing direction based on arrow key presses

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake=Snake()
+snake = Snake()
 
+screen.listen()
+screen.onkey(key="Up",fun=snake.up)
+screen.onkey(key="Down",fun=snake.down)
+screen.onkey(key="Left",fun=snake.left)
+screen.onkey(key="Right",fun=snake.right)
 
 game_is_on = True
 
