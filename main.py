428c0258cb6840a6d12df55d8c9cc500131e3ab0
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-07 13:17:20 +0530

    Created snake body

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+new_pos=0
+
+starting_positions=[(0,0),(-20,0),(-40,0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
 
 screen.exitonclick()
