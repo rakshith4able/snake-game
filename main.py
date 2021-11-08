41f129d9a57b4920c2811dee0b8b4efd014530b8
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-08 10:25:28 +0530

    Created food using seperate class Food and inheriting Turtle class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,12 +10,13 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
-screen.onkey(key="Up",fun=snake.up)
-screen.onkey(key="Down",fun=snake.down)
-screen.onkey(key="Left",fun=snake.left)
-screen.onkey(key="Right",fun=snake.right)
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 
