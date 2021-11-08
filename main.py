79f1c39d429665267b4945ec8489124160ba156b
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-08 11:11:53 +0530

    Added scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -20,13 +22,17 @@ screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 
+
+
 while game_is_on:
     screen.update()
     time.sleep(.1)
     snake.move()
 
-    #detect collision
-    if snake.head.distance(food)<15:
+    # detect collision
+    if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,10 +2,11 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-UP=90
-DOWN=270
-LEFT=180
-RIGHT=0
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
 
@@ -35,12 +36,12 @@ class Snake:
 
     def down(self):
         if self.head.heading() != UP:
-         self.head.setheading(DOWN)
+            self.head.setheading(DOWN)
 
     def left(self):
         if self.head.heading() != RIGHT:
-          self.head.setheading(LEFT)
+            self.head.setheading(LEFT)
 
     def right(self):
         if self.head.heading() != LEFT:
-         self.head.setheading(RIGHT)
+            self.head.setheading(RIGHT)
